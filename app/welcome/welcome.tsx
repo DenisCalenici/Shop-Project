c4959ef5171c17e7d88f8d5730ee3b64c764f896
DenisCalenici <DenisCalenici@gid.hab>
2025-05-16 19:17:32 +0300

    Changes to welcome

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -12,7 +12,8 @@ export function Welcome() {
 					<div className={s.header_advertisement}>
 						<div className={s.header_advertisement_discount}>Скидка 10% по промокоду “ZAMOK” на все заказы до 10.09</div>
 						<a className={s.header_advertisement_a}> Обратный звонок</a>
-					</div></div>
+					</div>
+				</div>
 				<div className={s.header_container_container}><div className={s.header_container}>
 					<div className={s.header_logo}>
 						<img src="./app/img/Logo (1).svg" />
@@ -663,12 +664,7 @@ export function Welcome() {
 						<div className={s.viewing_history_text}>
 							Вы недавно просмотрели
 						</div>
-						<div className={s.viewing_history_paginator}>
-							<a className={s.viewing_history_paginator_a1} href='#'>&lt;</a>
 
-
-							<a className={s.viewing_history_paginator_a2} href='#'>&gt;</a>
-						</div>
 
 					</div>
 
@@ -988,69 +984,72 @@ export function Welcome() {
 				</div>
 
 				<div className={s.ceo_product_mobile}>
-					<div className={s.product_block_mobile}>
+					<div className={s.ceo_product_mobile_block}>
 						<div className={s.product_block_mobile}>
-							<img className={s.product_block_mobile} src="app/img/white-3.png" />
+							<div className={s.product_block_mobile}>
+								<img className={s.product_block_mobile} src="app/img/white-3.png" />
+							</div>
+							<h3 className={s.ceo_product_h3_mobile}>
+								Eget quis quam metus, scelerisque.
+							</h3>
+							<div className={s.ceo_product_text_mobile}>
+								Odio felis sit leo, massa, mauris, at pulvinar
+								ultrices. Eu porttitor molestie massa porttitor.
+								Quisque at turpis ut proin eu et magna etiam
+								rhoncus.
+								<ul className={s.ul_text_mobile}>
+									<li className={s.li_text_mobile}>
+										<input type="checkbox" />
+										<p className={s.p_text_mobile}> Межкомнатую дверь</p>
+									</li>
+									<li className={s.li_text_mobile}>
+										<input type="checkbox" />
+										<p className={s.p_text_mobile} >Деревянную дверь</p>
+									</li>
+									<li className={s.li_text_mobile}>
+										<input type="checkbox" />
+										<p className={s.p_text_mobile}>Межкомнатую дверь</p>
+									</li>
+									<li className={s.li_text_mobile}>
+										<input type="checkbox" />
+										<p className={s.p_text_mobile}>Деревянную дверь</p>
+									</li>
+								</ul>
+							</div>
+							<div className={s.ceo_product_text_mobile}>
+								Ac risus neque pulvinar tincidunt est. Tristique
+								imperdiet viverra interdum in leo. Nullam
+								ullamcorper id enim fermentum integer praesent
+								bibendum. In ullamcorper purus scelerisque
+								malesuada et egestas. Ac dictumst mauris sed
+								facilisis.
+							</div>
 						</div>
-						<h3 className={s.ceo_product_h3_mobile}>
-							Eget quis quam metus, scelerisque.
-						</h3>
-						<div className={s.ceo_product_text_mobile}>
-							Odio felis sit leo, massa, mauris, at pulvinar
-							ultrices. Eu porttitor molestie massa porttitor.
-							Quisque at turpis ut proin eu et magna etiam
-							rhoncus.
-							<ul className={s.ul_text_mobile}>
-								<li className={s.li_text_mobile}>
-									<input type="checkbox" />
-									<p className={s.p_text_mobile}> Межкомнатую дверь</p>
-								</li>
-								<li className={s.li_text_mobile}>
-									<input type="checkbox" />
-									<p className={s.p_text_mobile} >Деревянную дверь</p>
-								</li>
-								<li className={s.li_text_mobile}>
-									<input type="checkbox" />
-									<p className={s.p_text_mobile}>Межкомнатую дверь</p>
-								</li>
-								<li className={s.li_text_mobile}>
-									<input type="checkbox" />
-									<p className={s.p_text_mobile}>Деревянную дверь</p>
-								</li>
-							</ul>
+
+						<div className={s.product_block_mobile}>
+							<img className={s.product_block_mobile} src="app/img/white-2.png" />
 						</div>
-						<div className={s.ceo_product_text_mobile}>
-							Ac risus neque pulvinar tincidunt est. Tristique
-							imperdiet viverra interdum in leo. Nullam
-							ullamcorper id enim fermentum integer praesent
-							bibendum. In ullamcorper purus scelerisque
-							malesuada et egestas. Ac dictumst mauris sed
-							facilisis.
+						<div className={s.product_block_mobile}>
+							<h3 className={s.ceo_product_h3_mobile}>
+								Eget quis quam metus, scelerisque.
+							</h3>
+							<div className={s.ceo_product_text_mobile}>
+								Odio felis sit leo, massa, mauris, at pulvinar
+								ultrices. Eu porttitor molestie massa porttitor.
+								Quisque at turpis ut proin eu et magna etiam
+								rhoncus.
+							</div>
+							<div className={s.ceo_product_text_mobile}>
+								Ac risus neque pulvinar tincidunt est. Tristique
+								imperdiet viverra interdum in leo. Nullam
+								ullamcorper id enim fermentum integer praesent
+								bibendum. In ullamcorper purus scelerisque
+								malesuada et egestas. Ac dictumst mauris sed
+								facilisis.
+							</div>
 						</div>
 					</div>
 
-					<div className={s.product_block_mobile}>
-						<img className={s.product_block_mobile} src="app/img/white-2.png" />
-					</div>
-					<div className={s.product_block_mobile}>
-						<h3 className={s.ceo_product_h3_mobile}>
-							Eget quis quam metus, scelerisque.
-						</h3>
-						<div className={s.ceo_product_text_mobile}>
-							Odio felis sit leo, massa, mauris, at pulvinar
-							ultrices. Eu porttitor molestie massa porttitor.
-							Quisque at turpis ut proin eu et magna etiam
-							rhoncus.
-						</div>
-						<div className={s.ceo_product_text_mobile}>
-							Ac risus neque pulvinar tincidunt est. Tristique
-							imperdiet viverra interdum in leo. Nullam
-							ullamcorper id enim fermentum integer praesent
-							bibendum. In ullamcorper purus scelerisque
-							malesuada et egestas. Ac dictumst mauris sed
-							facilisis.
-						</div>
-					</div>
 				</div>
 
 
@@ -1079,19 +1078,16 @@ export function Welcome() {
 			<div className={s.footer_body}>
 				<div className={s.footer}>
 					<div className={s.footer_logo}>
-						<div>
-							<img
-								className={s.footer_logo_img}
-								src="app/img/Logo (5).png"
-							/>
+						<img
+							className={s.footer_logo_img}
+							src="app/img/Logo (5).png"
+						/>
+						<img
+							className={s.footer_logo_img_2}
+							src="app/img/Frame 4075216.png"
+						/>
 
-						</div>
-						<div >
-							<img
-								className={s.footer_logo_img_2}
-								src="app/img/Frame 4075216.png"
-							/>
-						</div>
+
 					</div>
 					<div className={s.footer_ul}>
 						<ul className={s.footer_ul_ul}>
